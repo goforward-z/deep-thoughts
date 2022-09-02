e8cf00b0c6d2288d8e60eeb3c193dbe46071c74f
goforward-z <zlepeng.ucf@gmail.com>
2022-09-02 13:54:04 -0400

    concurrently

---
+++ client/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utlis/queries';
+import ThoughtList from '../components/ThoughtLis';
+
+const Home = () => {
+  //use useQuery hook to make query request
+  const { loading,data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>
+          {loading ? (
+            <div>loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title ="Some Feed for Thought(s)..."/>
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -25,6 +26,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+
+//server up static assets
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'../client/build')));
+}
+
+app.get('*',(req,res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
